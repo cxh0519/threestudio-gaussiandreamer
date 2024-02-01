abae4f76280538bfde42118c564b36e6f9e1e59f
cxh0519 <chengxinhua@stu.pku.edu.cn>
2024-02-01 14:18:20 +0800

    update

---
+++ README.md
@@ -0,0 +1,58 @@
+# threestudio-GaussianDreamer
+
+The [GaussianDreamer](https://github.com/hustvl/GaussianDreamer) extension for threestudio. This extension is writen by [Xinhua Cheng](https://github.com/cxh0519/). To use it, please install [threestudio](https://github.com/threestudio-project/threestudio) first and then install this extension in threestudio `custom` directory.
+
+## 🔧Install
+```
+cd custom
+git clone https://github.com/cxh0519/threestudio-gaussiandreamer.git
+cd threestudio-gaussiandreamer
+git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization
+git clone https://github.com/DSaurus/simple-knn.git
+pip install ./diff-gaussian-rasterization
+pip install ./simple-knn
+
+pip install open3d
+# If you want to export mesh, please install pymeshlab
+pip install pymeshlab
+```
+Please also install [MVDream extension](https://github.com/DSaurus/threestudio-mvdream), [shap-e extension](https://github.com/DSaurus/threestudio-shap-e) and my version of [lrm extension](https://github.com/cxh0519/threestudio-lrm) in threestudio `custom` directory with their instructions.
+
+**[Notice]** This extension is conflict with [threestudio-3dgs](https://github.com/DSaurus/threestudio-3dgs). Please make sure only one extension is actived.
+
+## 🚀Quick Start
+```
+# SD2.1 + shap-e initialize
+python launch.py --config custom/threestudio-gaussiandreamer/configs/gaussian_splatting.yaml  --train --gpu 0 system.prompt_processor.prompt="a fox" system.geometry.geometry_convert_from="shap-e:a fox"
+
+# MVDream + lrm initialize
+python launch.py --config custom/threestudio-gaussiandreamer/configs/gaussian_splatting_mvdream.yaml  --train --gpu 0 system.prompt_processor.prompt="an astronaut wearing a blue suit" system.geometry.geometry_convert_from="lrm:an astronaut"
+```
+**[Notice]** Different 2D diffusion guidances (SD, MVDream) and initialize methods (shap-e, lrm) can be conbined arbitrarily.
+
+## 📢Discussion
+
+**Difference with Official GaussianDreamer** 
+
+1. Slightly Hyper-pamarmeters differences.
+2. Support MVDream as 2D guidance.
+3. Support lrm initailzation.
+
+**Difference with threestudio-3dgs**
+
+1. Faster generation speed. (Around 10min on A100).
+2. Add Grow&Pertb technique proposed by GaussianDreamer.
+3. Support lrm+MVDream initailzation instead of lrm+SDXL.
+Compared to SDXL, MVDream generates more appropriate front/side view images for given text prompt.
+
+
+
+## 📌Citation
+```
+@article{GaussianDreamer,
+    title={GaussianDreamer: Fast Generation from Text to 3D Gaussian Splatting with Point Cloud Priors},
+    author={Taoran Yi and Jiemin Fang and Guanjun Wu and Lingxi Xie and Xiaopeng Zhang and Wenyu Liu and Qi Tian and Xinggang Wang},
+    journal={arxiv:2310.08529},
+    year={2023}
+}
+```
---
+++ renderer/diff_gaussian_rasterizer.py
@@ -0,0 +1,207 @@
+import math
+from dataclasses import dataclass
+
+import numpy as np
+import threestudio
+import torch
+import torch.nn.functional as F
+from diff_gaussian_rasterization import (
+    GaussianRasterizationSettings,
+    GaussianRasterizer,
+)
+from threestudio.models.background.base import BaseBackground
+from threestudio.models.geometry.base import BaseGeometry
+from threestudio.models.materials.base import BaseMaterial
+from threestudio.models.renderers.base import Rasterizer
+from threestudio.utils.typing import *
+
+from .gaussian_batch_renderer import GaussianBatchRenderer
+
+class Depth2Normal(torch.nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.delzdelxkernel = torch.tensor(
+            [
+                [0.00000, 0.00000, 0.00000],
+                [-1.00000, 0.00000, 1.00000],
+                [0.00000, 0.00000, 0.00000],
+            ]
+        )
+        self.delzdelykernel = torch.tensor(
+            [
+                [0.00000, -1.00000, 0.00000],
+                [0.00000, 0.00000, 0.00000],
+                [0.0000, 1.00000, 0.00000],
+            ]
+        )
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        delzdelxkernel = self.delzdelxkernel.view(1, 1, 3, 3).to(x.device)
+        delzdelx = F.conv2d(
+            x.reshape(B * C, 1, H, W), delzdelxkernel, padding=1
+        ).reshape(B, C, H, W)
+        delzdelykernel = self.delzdelykernel.view(1, 1, 3, 3).to(x.device)
+        delzdely = F.conv2d(
+            x.reshape(B * C, 1, H, W), delzdelykernel, padding=1
+        ).reshape(B, C, H, W)
+        normal = -torch.cross(delzdelx, delzdely, dim=1)
+        return normal
+
+
+@threestudio.register("diff-gaussian-rasterizer")
+class DiffGaussian(Rasterizer, GaussianBatchRenderer):
+    @dataclass
+    class Config(Rasterizer.Config):
+        debug: bool = False
+        invert_bg_prob: float = 1.0
+        back_ground_color: Tuple[float, float, float] = (1, 1, 1)
+
+    cfg: Config
+
+    def configure(
+        self,
+        geometry: BaseGeometry,
+        material: BaseMaterial,
+        background: BaseBackground,
+    ) -> None:
+        threestudio.info(
+            "[Note] Gaussian Splatting doesn't support material and background now."
+        )
+        super().configure(geometry, material, background)
+        self.background_tensor = torch.tensor(
+            self.cfg.back_ground_color, dtype=torch.float32, device="cuda"
+        )
+        self.normal_module = Depth2Normal()
+
+    def forward(
+        self,
+        viewpoint_camera,
+        bg_color: torch.Tensor,
+        scaling_modifier=1.0,
+        override_color=None,
+        **kwargs
+    ) -> Dict[str, Any]:
+        """
+        Render the scene.
+
+        Background tensor (bg_color) must be on GPU!
+        """
+        bg_color = bg_color * 0
+
+        pc = self.geometry
+        # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+        screenspace_points = (
+            torch.zeros_like(
+                pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda"
+            )
+            + 0
+        )
+        try:
+            screenspace_points.retain_grad()
+        except:
+            pass
+
+        # Set up rasterization configuration
+        tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+        tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+        raster_settings = GaussianRasterizationSettings(
+            image_height=int(viewpoint_camera.image_height),
+            image_width=int(viewpoint_camera.image_width),
+            tanfovx=tanfovx,
+            tanfovy=tanfovy,
+            bg=bg_color,
+            scale_modifier=scaling_modifier,
+            viewmatrix=viewpoint_camera.world_view_transform,
+            projmatrix=viewpoint_camera.full_proj_transform,
+            sh_degree=pc.active_sh_degree,
+            campos=viewpoint_camera.camera_center,
+            prefiltered=False,
+            debug=False,
+        )
+
+        rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+
+        means3D = pc.get_xyz
+        means2D = screenspace_points
+        opacity = pc.get_opacity
+
+        # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
+        # scaling / rotation by the rasterizer.
+        scales = None
+        rotations = None
+        cov3D_precomp = None
+        scales = pc.get_scaling
+        rotations = pc.get_rotation
+
+        # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
+        # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
+        shs = None
+        colors_precomp = None
+        if override_color is None:
+            shs = pc.get_features
+        else:
+            colors_precomp = override_color
+
+        # Rasterize visible Gaussians to image, obtain their radii (on screen).
+        rendered_image, radii, rendered_depth, rendered_alpha = rasterizer(
+            means3D=means3D,
+            means2D=means2D,
+            shs=shs,
+            colors_precomp=colors_precomp,
+            opacities=opacity,
+            scales=scales,
+            rotations=rotations,
+            cov3D_precomp=cov3D_precomp,
+        )
+        _, H, W = rendered_image.shape
+
+        batch_idx = kwargs["batch_idx"]
+        rays_d = kwargs["rays_d"][batch_idx]
+        rays_o = kwargs["rays_o"][batch_idx]
+        comp_rgb_bg = self.background(dirs=rays_d.unsqueeze(0))
+
+        rendered_image = rendered_image + (1 - rendered_alpha) * comp_rgb_bg.reshape(
+            H, W, 3
+        ).permute(2, 0, 1)
+        
+        xyz_map = rays_o + rendered_depth.permute(1, 2, 0) * rays_d
+        normal_map = self.normal_module(xyz_map.permute(2, 0, 1).unsqueeze(0))[0]
+        normal_map = F.normalize(normal_map, dim=0)
+        if pc.cfg.pred_normal:
+            pred_normal_map, _, _, _ = rasterizer(
+                means3D=means3D,
+                means2D=torch.zeros_like(means2D),
+                shs=pc.get_normal.unsqueeze(1),
+                colors_precomp=None,
+                opacities=opacity,
+                scales=scales,
+                rotations=rotations,
+                cov3D_precomp=cov3D_precomp,
+            )
+        else:
+            pred_normal_map = None
+        
+        normal_map = normal_map * 0.5 * rendered_alpha + 0.5
+        mask = rendered_alpha > 0.99
+        normal_mask = mask.repeat(3, 1, 1)
+        normal_map[~normal_mask] = normal_map[~normal_mask].detach()
+        rendered_depth[~mask] = rendered_depth[~mask].detach()
+
+        # Retain gradients of the 2D (screen-space) means for batch dim
+        if self.training:
+            screenspace_points.retain_grad()
+
+        # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+        # They will be excluded from value updates used in the splitting criteria.
+        return {
+            "render": rendered_image.clamp(0, 1),
+            "normal": normal_map,
+            "pred_normal": pred_normal_map,
+            "mask": rendered_alpha,
+            "depth": rendered_depth,
+            "viewspace_points": screenspace_points,
+            "visibility_filter": radii > 0,
+            "radii": radii,
+        }
